cda78b16ff3d9d0f99c45732a57de725e262349f
jab1015 <decideforusapp@gmail.com>
2026-03-31 20:36:24 -0400

    Fix location permission issue

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.decideforus.app"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 16
+        versionCode = 17
         versionName = "1.0.15"
     }
 
