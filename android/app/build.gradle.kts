9e65823af9a10b4216695a469ae3469e7f4880ed
jab1015 <decideforusapp@gmail.com>
2026-04-02 19:09:04 -0400

    Bump build to 19

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.decideforus.app"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 18
+        versionCode = 19
         versionName = "1.0.15"
     }
 
