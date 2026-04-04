3ef78657bbe55df8d38af6c8ba63cca86d1e94c9
jab1015 <decideforusapp@gmail.com>
2026-04-04 00:21:20 -0400

    Update version

--- lib/models/activity.dart
+++ lib/models/activity.dart
@@ -10,13 +10,4 @@ class Activity {
     required this.group,
     required this.budget,
   });
-
-  factory Activity.fromJson(Map<String, dynamic> json) {
-    return Activity(
-      title: json['title'] ?? '',
-      description: json['description'] ?? '',
-      group: json['group'] ?? '',
-      budget: json['budget'] ?? '',
-    );
-  }
 }
