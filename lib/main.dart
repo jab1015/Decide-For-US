30dc8b395c425271b33093b37592cbbaec65e66f
jab1015 <decideforusapp@gmail.com>
2026-07-29 17:49:04 -0400

    Update lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'screens/decide_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'app.dart';
+import 'firebase_options.dart';
+import 'services/subscription_service.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: DecideScreen(),
-    );
+  if (FirebaseAuth.instance.currentUser == null) {
+    await FirebaseAuth.instance.signInAnonymously();
   }
+
+  await SubscriptionService.init();
+  runApp(const DecideApp());
 }
